41e26794338ef72a3a7f3913bb02e2e4ff2a2f96
Ali <avida.chatraee2001@yahoo.com>
2021-05-29 11:33:22 +0430

    modal complete

--- assets/JS/controller.js
+++ assets/JS/controller.js
@@ -2,19 +2,18 @@ const addModalBox = (data) => {
   addArticlesOfValue(data);
 };
 function addArticlesOfValue(data) {
-  console.log(data["picture"]["large"]);
   const article = `<div class="col-12 col-sm-6 col-md-2">
     <img
       src="${data["picture"]["large"]}"
       class="btn p-1 avatar-button"
       data-bs-toggle="modal"
-      data-bs-target="#Modal"
+      data-bs-target="#${data["name"]["first"]}"
     />
 
     <!-- Modal -->
     <div
       class="modal fade"
-      id="Modal"
+      id="${data["name"]["first"]}"
       data-bs-backdrop="static"
       data-bs-keyboard="false"
       tabindex="-1"
@@ -24,7 +23,7 @@ function addArticlesOfValue(data) {
      <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <h5 class="modal-title" id="staticBackdropLabel">User Information</h5>
         <button
           type="button"
           class="btn-close"
@@ -32,7 +31,13 @@ function addArticlesOfValue(data) {
           aria-label="Close"
         ></button>
       </div>
-      <div class="modal-body">...</div>
+      <div class="modal-body text-center">
+        <img src="${data["picture"]["large"]}" class="avatar-button"></img>
+        <h3 class="mt-2" onclick="copySelectedText()">${data["name"]["title"]}. ${data["name"]["first"]} ${data["name"]["last"]}</h3>
+        <h5 class= "mt-2" onclick="copySelectedText()">City: ${data["location"]["city"]}</h5>
+        <h5 class= "mt-2" onclick="copySelectedText()">Email: ${data["email"]}</h5>
+        <h5 class= "mt-2" onclick="copySelectedText()">Phone: ${data["phone"]}</h5>
+      </div>
       <div class="modal-footer">
         <button
           type="button"
@@ -41,7 +46,6 @@ function addArticlesOfValue(data) {
         >
           Close
         </button>
-        <button type="button" class="btn btn-primary">Understood</button>
       </div>
     </div>
   </div>
@@ -49,3 +53,7 @@ function addArticlesOfValue(data) {
   </div>`;
   $(".data-row").append(article);
 }
+// Copy selected text
+const copySelectedText = () => {
+  document.execCommand("copy");
+};
